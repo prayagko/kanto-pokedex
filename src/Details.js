f05a99f512ae1e5dd2a921ede11dbf8db9d0efe8
Prayag <koprayag@gmail.com>
2021-11-25 21:51:01 +1100

    connected self-made backend

--- src/Details.js
+++ src/Details.js
@@ -1,21 +1,39 @@
 import React, {useState, useEffect} from 'react';
 
+
+
+
 function Details(props){
 
-    const api = 'https://pokeapi.co/api/v2/pokemon/'
+    const api = "https://002ow15i9i.execute-api.ap-southeast-2.amazonaws.com/dev/"
+
 
     const [pokemon, setPokemon] = useState({})
     const [loading, setLoading] = useState(true)
 
+
     async function requestPokemon(id) {
-        const response = await fetch(api+id)
+
+        const body = {
+            "pokemonId":id,
+            "fetchList":["name", "height", "weight", "types", "description", "sprites"],
+            "spritesList":["frontAnim","backAnim"]
+            }
+
+        const response = await fetch(api, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(body)
+            })
+
         if (response.ok){
-            const {name, stats, height, weight, abilities, types, species} = await response.json()
-            const speciesResponse = await fetch(species.url)
-            const {flavor_text_entries} = await speciesResponse.json()
-            const descriptionArray = flavor_text_entries.filter(d=>d.language.name==="en")
-            const description = descriptionArray[0].flavor_text
-            setPokemon({name, stats, height, weight, abilities, types, description}||{})
+            var {name, height, weight, types, description, sprites} = await response.json()
+            
+            sprites = JSON.parse(sprites)
+            types = JSON.parse(types)
+
+            setPokemon({name, height, weight, types, description, sprites}||{})
+            console.log('sprite', sprites.frontAnim)
         }
             setLoading(false)
       }
@@ -45,7 +63,7 @@ function Details(props){
                                     </div>
                                 </div>
                                 <div className="screen">
-                                    <img src={`/images/${props.id}.png`} alt={pokemon.name} className = "image"></img>
+                                    <img src={pokemon.sprites.frontAnim} alt={pokemon.name} className = "image"></img>
                                 </div>
                                 <div className="sideButtons">
                                     <div className="leftSideButton"></div>
@@ -84,7 +102,7 @@ function Details(props){
                                         Types
                                         </span>
                                         <span className="values">
-                                            {pokemon.types.map(t=><span key = {t.type.name} className={`types ${t.type.name}`}>{t.type.name}</span>)}
+                                            {pokemon.types.map(t=><span key = {t.name} className={`types ${t.name}`}>{t.name}</span>)}
                                         </span>
                                     </p>
                                     <p className="item">
